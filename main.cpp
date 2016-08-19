846e0062d0e307d31548a1ec5de187b3c1741850
Kostyantyn Kovalskyy <kostelkow@gmail.com>
2016-08-19 18:02:23 -0400

    Added temporary fix for hack eating up 100% CPU.

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ int main() {
         return 0;
     }
 
-    std::cout << "\n---------------[linux-csgo-external]---------------\n\n"                 << 
+    std::cout << "\n---------------[linux-csgo-external]---------------\n\n"                 <<
             "Original author: s0beit\n"                                       <<
             "Maintainers:\n\tlaazyboy13,\n\towerosu,\n\tMcSwaggens\n\tcommunity\n"    <<
             "github: https://github.com/McSwaggens/linux-csgo-external\n"   <<
@@ -42,15 +42,15 @@ int main() {
 
     Log::init();
     Log::put("Hack loaded...");
-    
+
     Display* display = XOpenDisplay(0);
 	Window root = DefaultRootWindow(display);
-	
+
     int keycodeGlow = XKeysymToKeycode(display, XK_F7);
     int keycodeFlash = XKeysymToKeycode(display, XK_F8);
     int keycodeBHopEnable = XKeysymToKeycode(display, XK_F9);
     int keycodeBHop = XKeysymToKeycode(display, XK_space);
-    
+
     unsigned int modifiers = 0;
     XGrabKey(display, keycodeGlow, modifiers, root, false,
 				GrabModeAsync, GrabModeAsync);
@@ -58,11 +58,11 @@ int main() {
 				GrabModeAsync, GrabModeAsync);
     XGrabKey(display, keycodeBHop, modifiers, root, false, GrabModeAsync, GrabModeAsync);
     XGrabKey(display, keycodeBHopEnable, modifiers, root, false, GrabModeAsync, GrabModeAsync);
-    
-    
+
+
     XSelectInput(display, root, KeyPressMask);
-    
-    
+
+
     remote::Handle csgo;
 
     while (true) {
@@ -103,17 +103,17 @@ int main() {
 
     cout << "Found client_client.so [" << std::hex << client.start << "]" << endl;
     client.client_start = client.start;
-    
+
     unsigned long pEngine = remote::getModule("engine_client.so", csgo.GetPid());
-    
+
     if (pEngine == 0)
     {
         cout << "Could not find engine module..." << endl;
         return 0;
     }
-    
+
     csgo.a_engine_client = pEngine;
-    
+
     cout << "Found engine_client.so: [" << pEngine << "]" << endl;
 
     void* foundGlowPointerCall = client.find(csgo,
@@ -130,13 +130,13 @@ int main() {
 
     csgo.m_addressOfGlowPointer = csgo.GetCallAddress((void*)(call+0xF));
     cout << "Glow Array pointer " << std::hex << csgo.m_addressOfGlowPointer << endl << endl;
-    
-    
+
+
     long ptrLocalPlayer = (client.client_start + 0x5A9B1A0); // 27/06/16
     unsigned long foundLocalPlayerLea = (long)client.find(csgo,
                                              "\x48\x89\xe5\x74\x0e\x48\x8d\x05\x00\x00\x00\x00", //27/06/16
                                              "xxxxxxxx????");
-                                             
+
     csgo.m_addressOfLocalPlayer = csgo.GetCallAddress((void*)(foundLocalPlayerLea+0x7));
 
     unsigned long foundAttackMov = (long)client.find(csgo,
@@ -147,20 +147,21 @@ int main() {
     unsigned long foundAlt1Mov = (long)client.find(csgo,
                                              "\x44\x89\xe8\xc1\xe0\x11\xc1\xf8\x1f\x83\xe8\x03\x45\x84\xe4\x74\x00\x21\xd0", //10/07/16
                                              "xxxxxxxxxxxxxxxx?xx");
-    
+
     csgo.m_addressOfAlt1 = csgo.GetCallAddress((void*)(foundAlt1Mov+20));
     cout << "Address of local player " << csgo.m_addressOfLocalPlayer << endl;
-    
+
     csgo.m_oAddressOfForceJump = client.client_start + 0x62F3500;
 
     csgo.m_bShouldGlow = true;
     csgo.m_bShouldNoFlash = true;
     csgo.m_bBhopEnabled = true;
     csgo.m_bShouldBHop = false;
-    
+
     XEvent ev;
-    
+
     while (csgo.IsRunning()) {
+       usleep(150);
 	    while (XPending(display) > 0) {
 			XNextEvent(display, &ev);
                 if (ev.type == KeyPress){
@@ -179,7 +180,7 @@ int main() {
                     if (ev.xkey.keycode == keycodeBHopEnable)
 					{
 						csgo.m_bBhopEnabled = !csgo.m_bBhopEnabled;
-                        
+
                         if (!csgo.m_bBhopEnabled)
                         {
                             XUngrabKey(display, keycodeBHop, modifiers, root);
@@ -188,7 +189,7 @@ int main() {
                         {
                             XGrabKey(display, keycodeBHop, modifiers, root, false, GrabModeAsync, GrabModeAsync);
                         }
-                        
+
                         cout << "BHop [" << Endi(csgo.m_bBhopEnabled) << "]" << endl;
 						break;
 					}
@@ -203,13 +204,13 @@ int main() {
                 {
                     break;
                 }
-                
+
 			XSelectInput(display, root, KeyPressMask);
 		}
         hack::Glow(&csgo, &client);
         hack::Bhop(&csgo, &client, display);
 	}
-	
+
     XUngrabKey(display, keycodeGlow, modifiers, root);
     XUngrabKey(display, keycodeFlash, modifiers, root);
     XUngrabKey(display, keycodeBHop, modifiers, root);
